11838cb2deeca6e90fb33fda1b2691f7b00ae488
Mike <Mpozdnjakov25@gmail.com>
2016-01-25 19:58:52 +0200

    new

--- src/com/Animal/App.java
+++ src/com/Animal/App.java
@@ -1,44 +1,52 @@
 package com.Animal;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Mike on 1/12/2016.
  */
 public class App {
     public static void main(String[] args) {
+
+        TreeMap <Integer,String>tree= new TreeMap();
+        tree.put(1,"Cat");
+        tree.put(2,"Dog");
+        System.out.println(tree.get(2));
+
         Animal bob = new Dog(1, 10, 20, "white", "Bob", "Buldog", true, true);
         Animal bob1 = new Dog(1, 10, 20, "white", "Bob", "Buldog", true, true);
-        Animal bob2 = new Dog(1, 10, 20, "black", "Bob","Buldog", true, true);
+        Animal bob2 = new Dog(1, 10, 20, "black", "Bob", "Buldog", true, true);
         // bob.voice();
 //        System.out.println();
-        Cat puss = new Cat(2, 10, 20, "gray", "Barsik","Pers", true);
+        Cat puss = new Cat(2, 10, 20, "gray", "Barsik", "Pers", true);
 //        puss.voice();
 //        System.out.println();
-        Wild giraffe = new Wild(3, 5, 100, "green","Giraff", false);
+        Wild giraffe = new Wild(3, 5, 100, "green", "Giraff", false);
 //        giraffe.voice();
 //        System.out.println();
-        Wild croc = new Wild(4, 5, 100, "green","Alligator", true);
+        Wild croc = new Wild(4, 5, 100, "green", "Alligator", true);
 //        croc.voice();
 //        System.out.println();
 //        Fish nemo=new Fish(1,2,3,"gold","Nemo",true);
 //        nemo.voice();
-        Map <Animal,Animal> zoo = new HashMap();
+        Map<String, Animal> zoo = new HashMap();
 
-        for (int i =0; i<1000; i++){
-            Animal bob3 = new Dog(i, i, i, "black", "Bob","Buldog", true, true);
+        int j=0;
+        for (int i = 0; i < 100000; i++) {
+            Animal bob3 = new Dog(i, i, i, "black", "Bob", "Buldog"+i, true, true);
 
-        zoo.put(bob3,bob3);
+            zoo.put("1"+i, bob3);
         }
-       double start = System.currentTimeMillis();
-        zoo.get(bob3).voice();
-        double stop = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            double start = System.currentTimeMillis();
+            zoo.get("1" + 99990);
+            double stop = System.currentTimeMillis();
 
 
-        System.out.println(start-stop);
+           if(stop-start==1.0)  j++;
+
+        }
+        System.out.println(j);
 //        List<Animal> arr = new ArrayList();
 //        arr.add(bob);
 //        arr.add(puss);
@@ -47,7 +55,6 @@ public class App {
 //        for (Animal item:arr) {
 //            item.voice();
 //        }
-
 
 
     }
